9ddd500bc437c7e0c0a56b58bcf3287a55d874b8
Dqrkky <hackerhere03@gmail.com>
2024-06-06 21:57:20 +0300

    master

--- shared.py
+++ shared.py
@@ -2,9 +2,7 @@ import urllib.parse
 
 class Shared:
     def __init__(self, rss=None):
-        self.rss = None
-        if rss != None:
-            self.rss=rss
+        self.rss = rss if rss else None
     def get_request_headers_json(self):
         if hasattr(self, "rss") and self.rss != None:
             return {
@@ -12,36 +10,39 @@ class Shared:
             }
     def set_request_headers(self, name :str=None, value :str=None):
         if hasattr(self, "rss") and self.rss != None:
-            if name != None and value != None:
+            if name and value != None:
                 self.rss.headers[name] = value
                 return self.get_request_headers_json()
     def remove_request_headers(self, name :str=None):
         if hasattr(self, "rss") and self.rss != None:
-            if name != None and name in self.get_request_headers_json():
+            if name and name in self.get_request_headers_json():
                 return {
                     "name": name,
                     "value": self.rss.headers.pop(name)
                 }
     def convert_json_to_values(self, config :dict=None):
-        if config != None and isinstance(config, dict):
+        if config and isinstance(config, dict):
             return (
-                config["method"] if "method" in config and config["method"] != None else None,
-                config["url"] if "url" in config and config["url"] != None else None,
-                config["params"] if "params" in config and config["params"] != None else None,
-                config["data"] if "data" in config and config["data"] != None else None,
-                config["headers"] if "headers" in config and config["headers"] != None else None,
-                config["cookies"] if "cookies" in config and config["cookies"] != None else None,
-                config["files"] if "files" in config and config["files"] != None else None,
-                config["auth"] if "auth" in config and config["auth"] != None else None,
-                config["timeout"] if "timeout" in config and config["timeout"] != None else None,
-                config["allow_redirects"] if "allow_redirects" in config and config["allow_redirects"] != None and isinstance(config["allow_redirects"], bool) else True,
-                config["proxies"] if "proxies" in config and config["proxies"] != None else None,
-                config["hooks"] if "hooks" in config and config["hooks"] != None else None,
-                config["stream"] if "stream" in config and config["stream"] != None and isinstance(config["stream"], bool) else False,
-                config["verify"] if "verify" in config and config["verify"] != None else None,
-                config["cert"] if "cert" in config and config["cert"] != None else None,
-                config["json"] if "json" in config and config["json"] != None else None,
+                config["method"] if "method" in config and config["method"] else None,
+                config["url"] if "url" in config and config["url"] else None,
+                config["params"] if "params" in config and config["params"] else None,
+                config["data"] if "data" in config and config["data"] else None,
+                config["headers"] if "headers" in config and config["headers"] else None,
+                config["cookies"] if "cookies" in config and config["cookies"] else None,
+                config["files"] if "files" in config and config["files"] else None,
+                config["auth"] if "auth" in config and config["auth"] else None,
+                config["timeout"] if "timeout" in config and config["timeout"] else None,
+                config["allow_redirects"] if "allow_redirects" in config and config["allow_redirects"] and isinstance(config["allow_redirects"], bool) else True,
+                config["proxies"] if "proxies" in config and config["proxies"] else None,
+                config["hooks"] if "hooks" in config and config["hooks"] else None,
+                config["stream"] if "stream" in config and config["stream"] isinstance(config["stream"], bool) else False,
+                config["verify"] if "verify" in config and config["verify"] else None,
+                config["cert"] if "cert" in config and config["cert"] else None,
+                config["json"] if "json" in config and config["json"] else None,
             )
     def dtsup(self, d1ct :dict=None):
-        if d1ct != None and isinstance(d1ct, dict):
+        if d1ct and isinstance(d1ct, dict):
             return urllib.parse.urlencode(query=d1ct)
+    def construct(self, url :str=None, params :str=None):
+        if url and isinstance(url, str) and params and isinstance(params, dict):
+            return f"{url}?{self.dtsup(params=params)}"
--- spotify.py
+++ spotify.py
@@ -34,12 +34,11 @@ class Spotify:
     def __init__(self, token: str = None):
         self.event_handlers = {}
         self.config = {
-            "spotify": {
-                "getaway": {
-                    "websocket": "wss://dealer.spotify.com"
-                },
-                "token": token if token != None and isinstance(token, str) else None
-            }
+            "getaway": {
+                "api": "https://gew1-spclient.spotify.com"
+                "websocket": "wss://dealer.spotify.com"
+            },
+            "access_token": access_token if access_token and isinstance(access_token, str) else None
         }
         with requests.Session() as rss:
             self.rss = rss
@@ -47,7 +46,7 @@ class Spotify:
             rss=self.rss
         )
     def request(self, config :dict=None):
-        if config != None and isinstance(config, dict):
+        if config and isinstance(config, dict):
             req = self.rss.request(
                 *self.shared.convert_json_to_values(
                     config=config
@@ -55,9 +54,6 @@ class Spotify:
             )
             if req.status_code == 200:
                 return req
-    def construct(self, url :str=None, params :dict=None):
-        if url != None and isinstance(url, str) and params != None and isinstance(params, dict):
-            return f'{url}?{self.shared.dtsup(params)}'
     def event(self, event_type=None):
         return self._register_event(event_type=event_type)
     def _register_event(self, event_type=None):
@@ -67,86 +63,145 @@ class Spotify:
             if event_type.__name__ not in self.event_handlers:
                 self.event_handlers[event_type.__name__] = event_type
             return event_type
-    async def trigger_event(self, event_name, event_data):
-        if event_name in self.event_handlers:
-            await self.event_handlers[event_name](event_data)
-    async def send_ping(self, websocket):
-        while True:
-            await websocket.ping()
-            await self.trigger_event("on_websocket_ping", WebsocketPing(time=datetime.datetime.now()))
-            await asyncio.sleep(5)
-    async def receive_data(self, websocket):
-        while True:
-            data = await websocket.recv()
-            loaded_data = json.loads(data)
-            if loaded_data != None and isinstance(loaded_data, dict):
-                if "headers" in loaded_data and loaded_data["headers"] != None and isinstance(loaded_data["headers"], dict):
-                    if "Spotify-Connection-Id" in loaded_data["headers"] and loaded_data["headers"]["Spotify-Connection-Id"] != None and isinstance(loaded_data["headers"]["Spotify-Connection-Id"], str):
-                        verified = self.verifydevice(
-                            spotify_connection_id=loaded_data["headers"]["Spotify-Connection-Id"]
+    async def trigger_event(self, event_name=None, event_data=None):
+        if event_name and event_data:
+            if event_name in self.event_handlers:
+                await self.event_handlers[event_name](event_data)
+    async def send_ping(self, websocket=None):
+        if websocket:
+            while True:
+                await websocket.ping()
+                await self.trigger_event(
+                    event_name="on_websocket_ping",
+                    event_data=WebsocketPing(
+                      time=datetime.datetime.now()
+                    )
+                )
+                await asyncio.sleep(5)
+    async def receive_data(self, websocket=None):
+        if websocket:
+            while True:
+                data = await websocket.recv()
+                if data:
+                    try:
+                        loaded_data = json.loads(
+                            data
                         )
-                        if verified != None:
-                            await self.trigger_event("on_connection_verify", ConnectionVerify(data=verified))
-                    if "content-type" in loaded_data["headers"] and loaded_data["headers"]["content-type"] != None and isinstance(loaded_data["headers"]["content-type"], str):
-                        if "application/json" == loaded_data["headers"]["content-type"]:
-                            if "payloads" in loaded_data and loaded_data["payloads"] != None and isinstance(loaded_data["payloads"], list) and len(loaded_data["payloads"]) > 0:
-                                if loaded_data["payloads"][0] != None and isinstance(loaded_data["payloads"][0], dict):
-                                    await self.trigger_event("on_websocket_raw", WebsocketRaw(data=loaded_data["payloads"][0]))
-                    if "Content-Type" in loaded_data["headers"] and loaded_data["headers"]["Content-Type"] != None and isinstance(loaded_data["headers"]["Content-Type"], str):
-                        if "text/plain" == loaded_data["headers"]["Content-Type"]:
-                            if "payloads" in loaded_data and loaded_data["payloads"] != None and isinstance(loaded_data["payloads"], list) and len(loaded_data["payloads"]) > 0:
-                                if loaded_data["payloads"][0] != None and isinstance(loaded_data["payloads"][0], str):
-                                    await self.trigger_event("on_user_interaction_raw", UserInteraction(data=json.loads(loaded_data["payloads"][0])))
+                    except Exception as e:
+                        loaded_data = None
+                        print(f"Error Type : {tye(e).__name__}\nError : {e.__name__}")
+                    if loaded_data and isinstance(loaded_data, dict):
+                        if "headers" in loaded_data and loaded_data["headers"] and isinstance(loaded_data["headers"], dict):
+                            if "Spotify-Connection-Id" in loaded_data["headers"] and loaded_data["headers"]["Spotify-Connection-Id"] and isinstance(loaded_data["headers"]["Spotify-Connection-Id"], str):
+                                verified = self.verifydevice(
+                                    spotify_connection_id=loaded_data["headers"]["Spotify-Connection-Id"]
+                                )
+                                if verified:
+                                    await self.trigger_event(
+                                        event_name="on_connection_verify",
+                                        event_data=ConnectionVerify(
+                                            data=verified
+                                        )
+                                    )
+                            if "content-type" in loaded_data["headers"] and loaded_data["headers"]["content-type"] and isinstance(loaded_data["headers"]["content-type"], str):
+                                if "application/json" == loaded_data["headers"]["content-type"]:
+                                    if "payloads" in loaded_data and loaded_data["payloads"] and isinstance(loaded_data["payloads"], list) and len(loaded_data["payloads"]) > 0:
+                                        if loaded_data["payloads"][0] and isinstance(loaded_data["payloads"][0], dict):
+                                            await self.trigger_event(
+                                                event_name="on_websocket_raw",
+                                                event_data=WebsocketRaw(
+                                                    data=loaded_data["payloads"][0]
+                                                )
+                                            )
+                            if "Content-Type" in loaded_data["headers"] and loaded_data["headers"]["Content-Type"] and isinstance(loaded_data["headers"]["Content-Type"], str):
+                                if "text/plain" == loaded_data["headers"]["Content-Type"]:
+                                    if "payloads" in loaded_data and loaded_data["payloads"] and isinstance(loaded_data["payloads"], list) and len(loaded_data["payloads"]) > 0:
+                                        if loaded_data["payloads"][0] and isinstance(loaded_data["payloads"][0], str):
+                                            await self.trigger_event(
+                                                event_name="on_user_interaction_raw", 
+                                                event_data=UserInteraction(
+                                                    data=json.loads(
+                                                        loaded_data["payloads"][0]
+                                                    )
+                                                )
+                                            )
     async def start(self):
-        if self.config["spotify"]["token"] != None:
+        if hasattr(self, "config") and self.config and isinstance(self.config, dict) and \
+        "access_token" in self.config and self.config["access_token"] and isinstance(self.config["access_token"], dict) and \
+        "getaway" in self.config and self.config["getaway"] and isinstance(self.config["getaway"], dict) and \
+        "api" in self.config["getaway"] and self.config["getaway"]["api"] and isinstance(self.config["getaway"]["api"], str):
             async with websockets.connect(
-                self.construct(
-                    url=self.config["spotify"]["getaway"]["websocket"],
+                self.shared.construct(
+                    url=self.config["getaway"]["websocket"],
                     params={
-                        "access_token": self.config["spotify"]["token"]
+                        "access_token": self.config["access_token"]
                     }
                 )
             ) as websocket:
-                ping_task = asyncio.create_task(self.send_ping(websocket))
-                data_task = asyncio.create_task(self.receive_data(websocket))
-                await asyncio.gather(ping_task, data_task)
+                ping_task = asyncio.create_task(
+                    self.send_ping(
+                        websocket=websocket
+                    )
+                )
+                data_task = asyncio.create_task(
+                    self.receive_data(
+                        websocket=websocket
+                    )
+                )
+                await asyncio.gather(
+                    ping_task,
+                    data_task
+                )
     def generate_hex_string(self, length :int=None):
-        if length != None and isinstance(length, int):
+        if length and isinstance(length, int):
             hex_characters = '0123456789abcdef'
-            hex_string = ''.join(random.choice(hex_characters) for _ in range(length))
-            return hex_string
+            return ''.join(random.choice(hex_characters) for _ in range(length))
     def hex_to_ascii(self, hex_string :str=None):
-        if hex_string != None and isinstance(hex_string, str):
+        if hex_string and isinstance(hex_string, str):
             ascii_string = ''
             for i in range(0, len(hex_string), 2):
                 ascii_string += chr(int(hex_string[i:i+2], 16))
             return ascii_string
     def verifydevice(self, spotify_connection_id :str=None):
-        if self.config["spotify"]["token"] != None and spotify_connection_id != None and isinstance(spotify_connection_id, str):
-            config = {
-                "method": "put",
-                "url": f"https://gew1-spclient.spotify.com/connect-state/v1/devices/hobs_{self.hex_to_ascii(self.generate_hex_string(24))}",
-                "data": json.dumps({
-                    "member_type": "CONNECT_STATE",
-                    "device": {
-                        "device_info": {
-                            "capabilities": {
-                                "can_be_player": False,
-                                "hidden": True,
-                                "needs_full_player_state": True
+        if hasattr(self, "config") and self.config and isinstance(self.config, dict) and \
+        "access_token" in self.config and self.config["access_token"] and isinstance(self.config["access_token"], dict) and \
+        "getaway" in self.config and self.config["getaway"] and isinstance(self.config["getaway"], dict) and \
+        "api" in self.config["getaway"] and self.config["getaway"]["api"] and isinstance(self.config["getaway"]["api"], str):
+            if spotify_connection_id and isinstance(spotify_connection_id, str):
+                config = {
+                    "method": "put",
+                    "url": f"f{
+                        self.config["getaway"]["api"]
+                    }/connect-state/v1/devices/hobs_{
+                        self.hex_to_ascii(
+                            hex_string=self.generate_hex_string(
+                              length=24
+                            )
+                        )
+                    }",
+                    "data": json.dumps({
+                        "member_type": "CONNECT_STATE",
+                        "device": {
+                            "device_info": {
+                                "capabilities": {
+                                    "can_be_player": False,
+                                    "hidden": True,
+                                    "needs_full_player_state": True
+                                }
                             }
                         }
+                    }),
+                    "headers": {
+                        "Authorization": f'Bearer {self.config["access_token"]}',
+                        "X-Spotify-Connection-Id": spotify_connection_id
                     }
-                }),
-                "headers": {
-                    "Authorization": f'Bearer {self.config["spotify"]["token"]}',
-                    "X-Spotify-Connection-Id": spotify_connection_id
                 }
-            }
-            req = self.request(
-                config=config
-            )
-            if req != None:
-                return req.json()
+                req = self.request(
+                    config=config
+                )
+                if req:
+                    return req.json()
     def run(self):
-        asyncio.run(self.start())
+        asyncio.run(
+            self.start()
+        )
